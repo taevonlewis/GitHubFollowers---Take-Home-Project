7aba93d3bd4128d60550c49620297903439760d9
TaeVon Lewis <65370736+taevonlewis@users.noreply.github.com>
2023-10-01 22:16:03 -0400

    created a username error modal to handle empty username input

---
+++ GitHubFollowers - Take Home Project/UIViewController+Ext.swift
@@ -0,0 +1,19 @@
+//
+//  UIViewController+Ext.swift
+//  GitHubFollowers - Take Home Project
+//
+//  Created by TaeVon Lewis on 10/1/23.
+//
+
+import UIKit
+
+extension UIViewController {
+    func presentGFAlertOnMainThread(title: String, message: String, buttonTitle: String) {
+        DispatchQueue.main.async {
+            let alertVC = GFAlertVC(title: title, message: message, buttonTitle: buttonTitle)
+            alertVC.modalPresentationStyle = .overFullScreen
+            alertVC.modalTransitionStyle = .crossDissolve
+            self.present(alertVC, animated: true)
+        }
+    }
+}
