b93ed02b533fbf8418b7fef9a97d5f399b68b924
TaeVon Lewis <65370736+taevonlewis@users.noreply.github.com>
2023-10-02 10:09:28 -0400

    user and follower objects

---
+++ GitHubFollowers - Take Home Project/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GitHubFollowers - Take Home Project
+//
+//  Created by TaeVon Lewis on 10/2/23.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
---
+++ GitHubFollowers - Take Home Project/Model/User.swift
@@ -0,0 +1,22 @@
+//
+//  User.swift
+//  GitHubFollowers - Take Home Project
+//
+//  Created by TaeVon Lewis on 10/2/23.
+//
+
+import Foundation
+
+struct User: Codable {
+    var login: String
+    var avatarUrl: String
+    var name: String?
+    var location: String?
+    var bio: String?
+    var publicRepos: Int
+    var publicGists: Int
+    var htmlUrl: String
+    var following: Int
+    var followers: Int
+    var createdAt: String
+}
