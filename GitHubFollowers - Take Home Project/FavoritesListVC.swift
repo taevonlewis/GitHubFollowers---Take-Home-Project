fed00ad46f3c6e8d6c8956bd2fd1fdb972a4d5bc
TaeVon Lewis <65370736+taevonlewis@users.noreply.github.com>
2023-10-01 20:10:52 -0400

    added tab bar and tab bar item files

--- GitHubFollowers - Take Home Project/FavoritesListVC.swift
+++ GitHubFollowers - Take Home Project/FavoritesListVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  FavoritesListVC.swift
 //  GitHubFollowers - Take Home Project
 //
 //  Created by TaeVon Lewis on 10/1/23.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FavoritesListVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemPink
+
+        view.backgroundColor = .systemBlue
     }
-
-
 }
-
---
+++ GitHubFollowers - Take Home Project/SearchVC.swift
@@ -0,0 +1,18 @@
+//
+//  SearchVC.swift
+//  GitHubFollowers - Take Home Project
+//
+//  Created by TaeVon Lewis on 10/1/23.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPink
+    }
+
+}
