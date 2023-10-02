56816a4ac3819197095c50444b1ffcbb7ac34328
TaeVon Lewis <65370736+taevonlewis@users.noreply.github.com>
2023-10-02 12:22:22 -0400

    initial collection view setup

---
+++ GitHubFollowers - Take Home Project/Custom Views/GFAvatarImageView.swift
@@ -0,0 +1,30 @@
+//
+//  GFAvatarImageView.swift
+//  GitHubFollowers - Take Home Project
+//
+//  Created by TaeVon Lewis on 10/2/23.
+//
+
+import UIKit
+
+class GFAvatarImageView: UIImageView {
+    
+    let placeholderImage = UIImage(named: "avatar-placeholder")!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        clipsToBounds = true
+        image = placeholderImage
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
